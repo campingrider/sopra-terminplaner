086fd83430cdd7d6609b24a922f45f6f63c02c97
Janosch Zoller <janosch.zoller@gmx.de>
2016-01-12 01:56:51 +0100

    Update toolbox.js
    added descriptional commentary

--- js/toolbox.js
+++ js/toolbox.js
@@ -1,7 +1,12 @@
 // (C) 2010, Janosch Zoller
 // As part of JZCMS
 
+// Commentary (c) 2016
+
+// create global Toolbox object
 var Toolbox = {
+	// this function searches for Elements by className, optionally also filtered with tagName
+	// ancient variant of modern querySelectorAll(tagName+"."+className)
 	getElementsByClassName : function (className, tagName) {
 		var elms = tagName ? window.document.getElementsByTagName(tagName) : window.document.getElementsByTagName('*');
 		var ret = [];
@@ -21,6 +26,7 @@ var Toolbox = {
 		}
 		return ret;
 	},
+	// retrieve all classNames as Strings from elms className-Property - ancient variant of modern class-list-functions
 	getClassNames : function (elm) {
 		if (elm) {
 			if (elm.className) {
@@ -33,6 +39,7 @@ var Toolbox = {
 			} else { return false; }
 		} else { return false; }
 	},
+	// checks whether elm has className cN in its className - ancient variant of modern class-list-functions
 	hasClassName : function (elm,cN) {
 		var cNs = Toolbox.getClassNames(elm);
 		if (cNs.length > 0) {
@@ -41,6 +48,9 @@ var Toolbox = {
 			return false;
 		}
 	},
+	// checks whether search is inside arr; 
+	// index: boolean whether key of found entry should be returned instead of boolean - default: false
+	// findAll: boolean whether an array with all found entities should be returned instead of first finding - default: false
 	isInArray : function (arr,search,index,findAll) {
 		if (arr && arr.length > 0 && search && typeof(search) == 'string') {
 			var id, check = false, idAll = [];
@@ -67,6 +77,7 @@ var Toolbox = {
 			window.alert('Javascript -> Funktion: Toolbox.isInArray(): Fehler bei den übergebenen Argumenten.');
 		}
 	},
+	// similar to isInArray, assuming that param index from isInArray is set to true, but uses obj to search for attribute instead of array to search for value
 	isInObject : function (obj,search,findAll) {
 		if (obj && search && typeof(search) == 'string') {
 			var id, check = false, idAll = [];
@@ -90,7 +101,9 @@ var Toolbox = {
 			window.alert('Javascript -> Funktion: Toolbox.isInObject(): Fehler bei den übergebenen Argumenten.');
 		}
 	},
+	// central XMLHttpRequest, enabling Requests through central management rather than having to define a new one each time needed
 	xmlHttp : new XMLHttpRequest(),
+	// initialize xmlHttp - set up a queue of requests and start the cycling queueWorker
 	xmlHttp_init : function () {
 		//Toolbox.xmlHttp.open('POST', '/cms/xhr', true);
 		//Toolbox.xmlHttp.setRequestHeader('content-type','text/plain');
@@ -142,6 +155,12 @@ var Toolbox = {
 			},200
 		);
 	},
+	// add a new Request into the queue so it can wait for being processed by the queueWorker
+	// url: Target of the Request
+	// content: encoded String with content of the Request
+	// blockNext: enforce follow-up requests to wait for my answer (ensure synchronous handling)
+	// method: send via GET or POST?
+	// ct: Content-Type in which content is encoded (default: application/x-www-form-urlencoded)
 	xmlHttp_send : function (url,content,blockNext,method,ct) {	
 		if (url) {
 			blockNext = blockNext ? blockNext : 'false';
@@ -151,7 +170,8 @@ var Toolbox = {
 			xrf = Toolbox.xmlHttp_response_function;
 			Toolbox.xmlHttp.queue.push({ url:url,content:content,blockNext:blockNext,method:method,ct:ct,xrf:xrf });
 		}
-	},	
+	},
+	// the following functions represent the handlers for Request-Answers as well as their default values
 	xmlHttp_response_function : function () {
 		Toolbox.xmlHttp_response_function_standard();
 	},
@@ -161,6 +181,7 @@ var Toolbox = {
 		}
 		window.setTimeout(function () { window.location.reload(); }, 100);
 	},
+	// compute absolute URI if a relative URL (towards locationHref) is given 
 	href_rel2abs : function (locationHref,relHref) {
 		var loc = locationHref.split('?')[0].split('#')[0];
 		
@@ -182,6 +203,7 @@ var Toolbox = {
 		
 		return abs;
 	},
+	// Polyfill - get scrollTop/scrollLeft in all Browsers
 	getScrollXY : function () {
 		var scrOfX = 0, scrOfY = 0;
 		if( typeof( window.pageYOffset ) == 'number' ) {
@@ -199,15 +221,24 @@ var Toolbox = {
 		}
 		return [ scrOfX, scrOfY ];
 	},
+	// temporary var-Container
 	temp : {
 	}
 }
 
+// The following definition takes the methods from Toolbox.xmlHttp and wraps them into an object-oriented class Toolbox.jz_xhr
+// Using this, various instances of Objects like Toolbox.xmlHttp can be created and run parallely
+// We will only comment functions and methods not covered in the description of Toolbox.xmlHttp above.
+
+
+// Array - all instances are registered here automatically 
 Toolbox.jz_xhrs = [];
 
 Toolbox.jz_xhr = function() {
+	// see Toolbox.xmlHttp
 	this.xmlHttp = false;
 	
+	// see Toolbox.xmlHttp
 	this.workerFunction = function () {
 		if (this.xmlHttp.queue.length > 0) {
 			if ((this.xmlHttp.readyState == 4 || this.xmlHttp.readyState == 0) && !Toolbox.xmlHtpp_blockNewRequest)  {
@@ -233,6 +264,7 @@ Toolbox.jz_xhr = function() {
 		}
 	}
 	
+	// see Toolbox.xmlHttp
 	this.xmlHttp_init = function () {		
 		
 		this.xmlHttp = new XMLHttpRequest();
@@ -260,6 +292,8 @@ Toolbox.jz_xhr = function() {
 		Toolbox.jz_xhrs.push(this);
 	}
 	
+	// see Toolbox.xmlHttp
+	// args: variables that can be used inside response_functions, e.g. as static parameters for other functions
 	this.xmlHttp_send = function (url,content,blockNext,method,ct,args) {	
 		if (url) {
 			blockNext = blockNext ? blockNext : 'false';
@@ -271,6 +305,7 @@ Toolbox.jz_xhr = function() {
 		}
 	}	
 	
+	// see Toolbox.xmlHttp - it is important to check whether jz_xhr or its xmlHttp-Member is the caller of this function
 	this.xmlHttp_response_function = function () {
 		if (this instanceof Toolbox.jz_xhr) {
 			this.xmlHttp_response_function_standard();
@@ -279,28 +314,35 @@ Toolbox.jz_xhr = function() {
 		}
 	}
 	
+	// see Toolbox.xmlHttp
 	this.xmlHttp_response_function_standard = function () {
 		if (this.xmlHttp.responseText && this.xmlHttp.responseText !== '' && this.xmlHttp.responseText != ' ') {
 			alert(this.xmlHttp.responseText);
 		}
 	}
 
+	// Shortcut
 	this.send = function (url,content,blockNext,method,ct,args) {
 		this.xmlHttp_send(url,content,blockNext,method,ct,args);
 	}
 	
+	// Shortcut
 	this.init = function() {
 		this.xmlHttp_init();
 	}
 	
+	// Shortcuts
 	this.response_function_standard = this.xmlHttp_response_function_standard;
 	this.response_function = this.xmlHttp_response_function;
 	
+	// initialize
 	this.init();
 }
 
+// initialize static Toolbox.xmlHttp
 Toolbox.xmlHttp_init();
 
+// enable Worker-Functions; cycle with 200 ms delay
 window.setInterval(
 	function() {
 		for (var i = 0; i < Toolbox.jz_xhrs.length; i++) {
